f60c012e208e739518141710769c198460be6826
T H A T  B O I <43853225+Devpineapple@users.noreply.github.com>
2020-08-11 14:20:10 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,77 @@
+let mySprite = sprites.create(img`
+    f f f f f f f f f f f f f f f f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 f 5 5 5 5 5 5 5 f 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 f f f 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f f f f f f f f f f f f f f f f 
+    `, SpriteKind.Player)
+let mySprite2 = sprites.create(img`
+    f f f f f f f f f f f f f f f f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 f 2 2 2 f 2 2 2 f 
+    f 2 2 2 2 2 2 2 f 2 f 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 f 2 2 2 2 2 f 
+    f 2 2 2 2 f 2 2 2 2 2 2 f 2 2 f 
+    f 2 2 2 2 2 2 2 f f f 2 2 2 2 f 
+    f 2 2 2 2 2 2 f 2 2 2 f 2 2 2 f 
+    f 2 2 2 2 2 2 f 2 2 2 f 2 2 2 f 
+    f 2 2 2 2 2 2 f 2 2 2 f 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f f f f f f f f f f f f f f f f 
+    `, SpriteKind.Player)
+let mySprite3 = sprites.create(img`
+    f f f f f f f f f f f f f f f f 
+    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+    f 4 4 4 4 f f f f f f 4 4 4 4 f 
+    f 4 4 4 4 f f f f f f f 4 4 4 f 
+    f 4 4 4 4 f f 4 4 4 f f 4 4 4 f 
+    f 4 4 4 4 f f f f f f f 4 4 4 f 
+    f 4 4 4 4 f f f f f f 4 4 4 4 f 
+    f 4 4 4 4 f f 4 4 4 4 4 4 4 4 f 
+    f 4 4 4 4 f f 4 4 4 4 4 4 4 4 f 
+    f 4 4 4 4 f f 4 4 4 4 4 4 4 4 f 
+    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+    f f f f f f f f f f f f f f f f 
+    `, SpriteKind.Player)
+let mySprite4 = sprites.create(img`
+    f f f f f f f f f f f f f f f f 
+    f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+    f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+    f 7 7 7 7 f f f f f 7 7 7 7 7 f 
+    f 7 7 7 f f f f f f 7 7 7 7 7 f 
+    f 7 7 f f f 7 7 7 7 7 7 7 7 7 f 
+    f 7 7 f f 7 7 7 7 7 7 7 7 7 7 f 
+    f 7 7 f f 7 f f f f f f 7 7 7 f 
+    f 7 7 f f 7 f f f f f f f 7 7 f 
+    f 7 7 f f 7 7 7 7 7 7 f f 7 7 f 
+    f 7 7 f f f 7 7 7 7 f f 7 7 7 f 
+    f 7 7 7 f f f f f f f f 7 7 7 f 
+    f 7 7 7 7 f f f f f f 7 7 7 7 f 
+    f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+    f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+    f f f f f f f f f f f f f f f f 
+    `, SpriteKind.Player)
+mySprite.setPosition(randint(15, 160), randint(15, 200))
+mySprite2.setPosition(randint(15, 160), randint(15, 200))
+mySprite3.setPosition(randint(15, 160), randint(15, 200))
+mySprite4.setPosition(randint(15, 160), randint(15, 200))
+scene.cameraFollowSprite(mySprite)
